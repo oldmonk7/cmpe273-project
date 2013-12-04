f3b8dba5e57a62f74bfe64986aac4e9ab429110b
Arun Malik <malik@Aruns-MacBook-Pro.local>
2013-12-03 23:09:27 -0800

    implemented Session management using AuthToken, changed plan schema

--- src/main/java/com/eventplanner/api/resources/LoginResource.java
+++ src/main/java/com/eventplanner/api/resources/LoginResource.java
@@ -1,101 +1,65 @@
 package com.eventplanner.api.resources;
 
-import com.eventplanner.domain.StakeHolders;
-import com.eventplanner.domain.Users;
-import com.yammer.metrics.annotation.Timed;
-
 import javax.validation.Valid;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/signup")
+import com.eventplanner.domain.Users;
+import com.sun.jersey.api.core.HttpRequestContext;
+import com.yammer.metrics.annotation.Timed;
+
+@Path("/auth")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class LoginResource {
 
-    public LoginResource() {
-    }
-    
-    
-    @GET
-	@Timed(name = "view-stakeholders")
-	 public Response getStakeHolders() {
-		try{
-			//StakeHolders.getStakeHolders();
-			
-			
-			return Response.status(200).entity("{msg : CustomerCreated}").build();
-		}
-		catch(Exception ex)
-		{
-			return Response.status(Response.Status.BAD_REQUEST).entity("Invalid Parameters " + ex.getMessage()).build();
-		}
-		finally
-		{
-			if(null != null)
-			{
-				//null = null;
-			}
-		}
-    }
-
-    @POST
-    @Path("/serviceprovider")
-	@Timed(name = "create-stakeholders")
-    public Response saveStakeHolders(@Valid StakeHolders stakeHolderToCreate) {
-		try{
-             String msg="{\"msg\":\"StakeHolder Created\"}";
-			return Response.status(201).entity(msg).build();
-		}
-		catch(Exception ex)
-		{
-			return Response.status(Response.Status.BAD_REQUEST).entity("Invalid Parameters " + ex.getMessage()).build();
-		}
-		finally
-		{
-			if(null != null)
-			{
-				//null = null;
-			}
-		}
-    }
-    
-    @POST
-    @Path("/customer")
-	@Timed(name = "create-user")
-    public Response saveUser(@Valid Users userToCreate) {
-		try{
-			userToCreate.saveUser();
-            String msg = "{\"msg\":\"Customer Created\"}";
-            System.out.println((Response.status(201).entity(msg).build().getEntity()));
+	public LoginResource() {
+	}
 
 
-			return Response.status(201).entity(msg).build();
-		}
-		catch(Exception ex)
-		{
-			return Response.status(Response.Status.BAD_REQUEST).entity("Invalid Parameters " + ex.getMessage()).build();
-		}
-		finally
-		{
-			if(null != null)
-			{
-				//null = null;
-			}
-		}
-    }
-    
-    @GET
-    @Path("/customer")
-	@Timed(name = "create-user")
-    public Response getUsers() {
+	@POST
+	@Path("/login")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Timed(name = "login-users")
+	public Response loginUser(@HeaderParam("Email") String email, @HeaderParam("Password") String password) {
+		Users obj = new Users();
+
 		try{
-			Users obj = new Users();
-			//Iterable<Users> user = obj.getUsers();
-			
-			
-			return Response.status(200).entity(obj.findUsers("{name: \"Arun\"}")).build();
+			if(null!= email || null!= password)
+			{
+
+				//	Users user = new Users();
+				obj  = obj.findUserByEmail(email);
+
+				if(null != obj)
+				{
+					if(obj.getPassword().equals(password))
+					{
+						obj.addUserToken();
+						return Response.status(Response.Status.OK).entity(obj.toResponseList()).build();
+					}
+					else
+					{
+						return Response.status(Response.Status.UNAUTHORIZED).entity("Password not valid").build();
+					}
+
+				}
+				else
+				{
+					return Response.status(Response.Status.UNAUTHORIZED).entity("User Id not valid").build();
+				}
+			}else
+			{
+				return Response.status(Response.Status.BAD_REQUEST).entity("Invalid Parameters ").build();
+			}
+
 		}
 		catch(Exception ex)
 		{
@@ -103,12 +67,132 @@ public class LoginResource {
 		}
 		finally
 		{
-			if(null != null)
+			if(obj != null)
 			{
-				//null = null;
+				obj= null;
 			}
 		}
-    }
-   
+	}
+
+
+	@POST
+	@Path("/logout")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Timed(name = "logout-users")
+	public Response logoutUser(@HeaderParam("Auth-Token") String token) {
+		Users obj = new Users();
+
+		if(obj.isUserTokenValid(token))
+		{
+			obj.deleteUserToken(token);;
+			return Response.status(Response.Status.OK).entity("{\"msg:\" : \"Logged out Successfully\"}").build();
+		}
+		else
+		{
+			return Response.status(Response.Status.UNAUTHORIZED).entity("Unauthorized access").build();
+		}
+
+
+
+	}
+	//    
+	//    @GET
+	//	@Timed(name = "view-stakeholders")
+	//	 public Response getStakeHolders() {
+	//		try{
+	//			//StakeHolders.getStakeHolders();
+	//			
+	//			
+	//			return Response.status(200).entity("{msg : CustomerCreated}").build();
+	//		}
+	//		catch(Exception ex)
+	//		{
+	//			return Response.status(Response.Status.BAD_REQUEST).entity("Invalid Parameters " + ex.getMessage()).build();
+	//		}
+	//		finally
+	//		{
+	//			if(null != null)
+	//			{
+	//				//null = null;
+	//			}
+	//		}
+	//    }
+	//
+	//    @POST
+	//    @Path("/serviceprovider")
+	//	@Timed(name = "create-stakeholders")
+	//    public Response saveStakeHolders(@Valid StakeHolders stakeHolderToCreate) {
+	//		try{
+	//             String msg="{\"msg\":\"StakeHolder Created\"}";
+	//			return Response.status(201).entity(msg).build();
+	//		}
+	//		catch(Exception ex)
+	//		{
+	//			return Response.status(Response.Status.BAD_REQUEST).entity("Invalid Parameters " + ex.getMessage()).build();
+	//		}
+	//		finally
+	//		{
+	//			if(null != null)
+	//			{
+	//				//null = null;
+	//			}
+	//		}
+	//    }
+	//    
+	//    @POST
+	//    @Path("/customer")
+	//	@Timed(name = "create-user")
+	//    public Response saveUser(@Valid Users userToCreate) {
+	//		try{
+	//			userToCreate.addUserToken();
+	//			userToCreate.saveUser();
+	//           
+	//			String msg = "{\"msg\":\"Customer Created\"}";
+	//           
+	//            System.out.println((Response.status(201).entity(msg).build().getEntity()));
+	//
+	//
+	//			return Response.status(201).entity(msg).build();
+	//		}
+	//		catch(Exception ex)
+	//		{
+	//			return Response.status(Response.Status.BAD_REQUEST).entity("Invalid Parameters " + ex.getMessage()).build();
+	//		}
+	//		finally
+	//		{
+	//			if(null != null)
+	//			{
+	//				//null = null;
+	//			}
+	//		}
+	//    }
+	//    
+	//
+	//
+	//	@GET
+	//    @Path("/customer")
+	//	@Timed(name = "create-user")
+	//    public Response getUsers() {
+	//		try{
+	//			Users obj = new Users();
+	//			//Iterable<Users> user = obj.getUsers();
+	//			
+	//			
+	//			return Response.status(200).entity(obj.getUsers()).build();
+	//		}
+	//		catch(Exception ex)
+	//		{
+	//			return Response.status(Response.Status.BAD_REQUEST).entity("Invalid Parameters " + ex.getMessage()).build();
+	//		}
+	//		finally
+	//		{
+	//			if(null != null)
+	//			{
+	//				//null = null;
+	//			}
+	//		}
+	//    }
+
 }
 
--- src/main/java/com/eventplanner/api/resources/UserResource.java
+++ src/main/java/com/eventplanner/api/resources/UserResource.java
@@ -16,10 +16,13 @@ public class UserResource {
 
 	@POST
 	@Timed(name = "create-user")
-    public Response saveUser(@Valid Users userToCreate) {
+	public Response saveUser(@Valid Users userToCreate) {
+
 		try{
+			userToCreate.setType("customer");
+			userToCreate.addUserToken();
 			userToCreate.saveUser();
-			return Response.status(201).entity("User Created : \n"+ userToCreate.toList()).build();
+			return Response.status(201).entity("User Created : \n"+ userToCreate.toResponseList()).build();
 		}
 		catch(Exception ex)
 		{
@@ -32,53 +35,69 @@ public class UserResource {
 				userToCreate= null;
 			}
 		}
-    }
-    
-    @GET
+
+	}
+
+	@GET
 	@Timed(name = "get-user")
-    public Response getUsers() {
-    	Iterable<Users> user  =null;
-    	Users userObj = new Users();
-		try{
-			user = userObj.getUsers();			
-			return Response.status(200).entity(Lists.newArrayList(user)).build();
-		}
-		catch(Exception ex)
+	public Response getUsers(@HeaderParam("Auth-Token") String token) {
+		Iterable<Users> user  =null;
+		Users userObj = new Users();
+		if(userObj.isUserTokenValid(token))
 		{
-			return Response.status(Response.Status.BAD_REQUEST).entity("Invalid Parameters " + ex.getMessage()).build();
+			try{
+				user = userObj.getUsers();			
+				return Response.status(200).entity(Lists.newArrayList(user)).build();
+			}
+			catch(Exception ex)
+			{
+				return Response.status(Response.Status.BAD_REQUEST).entity("Invalid Parameters " + ex.getMessage()).build();
+			}
+			finally
+			{
+				if(user != null)
+				{
+					user = null;
+				}
+				if(userObj != null)
+				{
+					userObj = null;
+				}
+			}
 		}
-		finally
+		else
 		{
-			if(user != null)
-			{
-				user = null;
-			}
-			if(userObj != null)
-			{
-				userObj = null;
-			}
+			return Response.status(Response.Status.UNAUTHORIZED).entity("Unauthorized access").build();
 		}
-    }
-    
-    @GET
-    @Path("/{name}")
+	}
+
+	@GET
+	@Path("/{name}")
 	@Timed(name = "get-user")
-    public Response getUser(@PathParam("name") String name) {
-    	Users userObj = new Users();
-		try{
-			
-			return Response.status(200).entity( userObj.findUserByName(name)).build();
-		}
-		catch(Exception ex)
+	public Response getUser(@PathParam("name") String name,@HeaderParam("Auth-Token") String token) {
+		Users userObj = new Users();
+		if(userObj.isUserTokenValid(token))
 		{
-			return Response.status(Response.Status.BAD_REQUEST).entity("Invalid Parameters " + ex.getMessage()).build();
-		}
-		finally
-		{
-			if(userObj != null)
+			try{
+
+				return Response.status(200).entity( userObj.findUserByName(name)).build();
+			}
+			catch(Exception ex)
 			{
-				userObj = null;
+				return Response.status(Response.Status.BAD_REQUEST).entity("Invalid Parameters " + ex.getMessage()).build();
+			}
+			finally
+			{
+				if(userObj != null)
+				{
+					userObj = null;
+				}
 			}
 		}
-    }
+		else
+		{
+			return Response.status(Response.Status.UNAUTHORIZED).entity("Unauthorized access").build();
+		}
+
+	}
 }
